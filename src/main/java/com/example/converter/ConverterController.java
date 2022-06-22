aaeb5969018b63f3d521933f49d9264df837d944
safwan zullash <wi21b030@technikum-wien.at>
2022-06-22 16:30:03 +0200

    added stones to kilo function added ounces to gram function added Class Converter

--- src/main/java/com/example/converter/ConverterController.java
+++ src/main/java/com/example/converter/ConverterController.java
@@ -1,13 +1,25 @@
 package com.example.converter;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConverterController {
 
+    private Converter conv = new Converter();
     @RequestMapping("/")
     public String index(){
-        return "Wilkommen zum Converter!";
+        return conv.index();
+    }
+
+    @RequestMapping("/kilo")
+    public String kilo(@RequestParam double st){
+        return String.format("%d Stone/s sind %d Kilo/s",st,conv.kilo(st));
+    }
+
+    @RequestMapping("/gram")
+    public String gram(@RequestParam double ou){
+        return String.format("%d Ounce/s sind %d Gramm",ou,conv.gram(ou));
     }
 }
